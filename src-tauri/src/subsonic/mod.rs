3a49a3f31293187b2eca6fd5775642366bb3a8e9
bbachdev <bbach7139@gmail.com>
2024-12-30 10:44:59 -0600

    Add reference to "ping"; remove unnecessary borrow

--- src-tauri/src/subsonic/mod.rs
+++ src-tauri/src/subsonic/mod.rs
@@ -2,8 +2,10 @@ use crate::formatter::create_connection_string;
 use crate::models::Library;
 use crate::responses::{SubsonicBaseResponse, SubsonicResponse};
 
+/* Ping
+* https://opensubsonic.netlify.app/docs/endpoints/ping */
 pub async fn ping_server(library: &Library) -> Result<(), anyhow::Error> {
-    let url = create_connection_string(&library, "ping");
+    let url = create_connection_string(library, "ping");
     match reqwest::get(&url).await {
         Ok(res) => match res.json::<SubsonicResponse<SubsonicBaseResponse>>().await {
             Ok(_) => Ok(()),
