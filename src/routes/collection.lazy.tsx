01c8a1e021bfe9be233db554fff88ddb676f4506
bbachdev <bbach7139@gmail.com>
2025-05-14 12:18:12 -0500

    Persist NowPlaying when viewing Settings menu

--- src/routes/collection.lazy.tsx
+++ src/routes/collection.lazy.tsx
@@ -87,22 +87,22 @@ function Collection() {
   useEffect(() => {
     async function syncLibraries() {
       //Check if we need to sync
-      if (libraries && libraries.size > 0) {
-        const library_data = Array.from(libraries.values())
-        if(await library_modified(library_data) === true) {
-          setIsScanning(true)
-          await invoke('sync_collection', { libraries: library_data })
-            .then(() => {
-              console.log("Synced")
-              setIsScanning(false)
-            }).catch((e) => {
-              console.log("==Error: ", e)
-              console.log("Failed to sync")
-            })
-        }else{
-          console.log("Library not modified")
-        }
-      }
+      // if (libraries && libraries.size > 0) {
+      //   const library_data = Array.from(libraries.values())
+      //   if(await library_modified(library_data) === true) {
+      //     setIsScanning(true)
+      //     await invoke('sync_collection', { libraries: library_data })
+      //       .then(() => {
+      //         console.log("Synced")
+      //         setIsScanning(false)
+      //       }).catch((e) => {
+      //         console.log("==Error: ", e)
+      //         console.log("Failed to sync")
+      //       })
+      //   }else{
+      //     console.log("Library not modified")
+      //   }
+      // }
     }
     //Check if we need to sync
     syncLibraries()
@@ -110,64 +110,68 @@ function Collection() {
 
   return (
     <QueueContextProvider>
-      { overallPage === 'collection' && (
-        <div className={`w-full flex flex-col`}>
-          <div className={`flex flex-row p-4 items-center border-b-2 border-slate-800 dark:border-slate-200`}>
-            <div>
-              <img src='/tauri.svg' className={`h-10 w-10`} />
+      <div className={`w-full flex flex-col`}>
+        { overallPage === 'collection' && (
+          <>   
+            <div className={`flex flex-row p-4 items-center border-b-2 border-slate-800 dark:border-slate-200`}>
+              <div>
+                <img src='/tauri.svg' className={`h-10 w-10`} />
+              </div>
+              <div className={`ml-auto`}>
+                <button onClick={() => setOverallPage('settings')}><FaGear className={`h-8 w-8`} /></button>
+              </div>
             </div>
-            <div className={`ml-auto`}>
-              <button onClick={() => setOverallPage('settings')}><FaGear className={`h-8 w-8`} /></button>
-            </div>
-          </div>
-          { isScanning && (
-            <div className={`w-full py-1 flex flex-row items-center justify-center gap-2 bg-slate-900`}>
-              <Spinner size={16} className={`mt-1`} />
-              <span>Syncing Collection...</span>
-            </div>
-          )}
-          <ResizablePanelGroup direction='horizontal'>
-            <ResizablePanel defaultSize={20} minSize={15}>
-              { leftView === 'artist' && (
-                <ArtistList onArtistSelected={(artistId) => setCurrentArtistId(artistId)} onPlaylistClicked={() =>toggleLeftView('playlist')} />
-              )}
-              { leftView === 'playlist' && (
-                <PlaylistList onPlaylistSelected={getPlaylistSongs} onTagClicked={() => toggleLeftView('tag')} />
-              )}
-              { leftView === 'tag' && (
-                <TagList onTagSelected={(tagId) => setcurrentTagId(tagId)} onArtistClicked={() => toggleLeftView('artist')} />
-              )}
-            </ResizablePanel>
-            { (leftView === 'artist' || leftView === 'tag') && (
-              <>
-                <ResizableHandle className={`dark:bg-slate-200`} />
-                <ResizablePanel defaultSize={58} minSize={30}>
-                  <AlbumList libraries={libraries || new Map<String, Library>()} parentAlbums={((leftView === 'artist') ? artistAlbums : tagAlbums) || []} onAlbumSelected={getAlbumSongs} view={leftView} />
-                </ResizablePanel>
-              </>   
+            { isScanning && (
+              <div className={`w-full py-1 flex flex-row items-center justify-center gap-2 bg-slate-900`}>
+                <Spinner size={16} className={`mt-1`} />
+                <span>Syncing Collection...</span>
+              </div>
             )}
-            <ResizableHandle className={`dark:bg-slate-200`} />
-
-            { leftView === 'playlist' && (
-              <>
-                <ResizablePanel defaultSize={58} minSize={30}>
-                <SongList nowPlayingId={nowPlayingId} songs={(!isPlaylistSongsLoading && !isAlbumSongsLoading) ? songList : []} listInfo={selectedListInfo} mode={'playlist'} />
-                </ResizablePanel>
-              </>
-            )}
-            { (leftView === 'artist' || leftView === 'tag') && (
-              <ResizablePanel defaultSize={22} minSize={20}>
-                <SongList nowPlayingId={nowPlayingId} songs={(!isPlaylistSongsLoading && !isAlbumSongsLoading) ? songList : []} listInfo={selectedListInfo} mode={'artist'} />
+            <ResizablePanelGroup direction='horizontal'>
+              <ResizablePanel defaultSize={20} minSize={15}>
+                { leftView === 'artist' && (
+                  <ArtistList onArtistSelected={(artistId) => setCurrentArtistId(artistId)} onPlaylistClicked={() =>toggleLeftView('playlist')} />
+                )}
+                { leftView === 'playlist' && (
+                  <PlaylistList onPlaylistSelected={getPlaylistSongs} onTagClicked={() => toggleLeftView('tag')} />
+                )}
+                { leftView === 'tag' && (
+                  <TagList onTagSelected={(tagId) => setcurrentTagId(tagId)} onArtistClicked={() => toggleLeftView('artist')} />
+                )}
               </ResizablePanel>
-            )}
-          </ResizablePanelGroup>
+              { (leftView === 'artist' || leftView === 'tag') && (
+                <>
+                  <ResizableHandle className={`dark:bg-slate-200`} />
+                  <ResizablePanel defaultSize={58} minSize={30}>
+                    <AlbumList libraries={libraries || new Map<String, Library>()} parentAlbums={((leftView === 'artist') ? artistAlbums : tagAlbums) || []} onAlbumSelected={getAlbumSongs} view={leftView} />
+                  </ResizablePanel>
+                </>   
+              )}
+              <ResizableHandle className={`dark:bg-slate-200`} />
+
+              { leftView === 'playlist' && (
+                <>
+                  <ResizablePanel defaultSize={58} minSize={30}>
+                  <SongList nowPlayingId={nowPlayingId} songs={(!isPlaylistSongsLoading && !isAlbumSongsLoading) ? songList : []} listInfo={selectedListInfo} mode={'playlist'} />
+                  </ResizablePanel>
+                </>
+              )}
+              { (leftView === 'artist' || leftView === 'tag') && (
+                <ResizablePanel defaultSize={22} minSize={20}>
+                  <SongList nowPlayingId={nowPlayingId} songs={(!isPlaylistSongsLoading && !isAlbumSongsLoading) ? songList : []} listInfo={selectedListInfo} mode={'artist'} />
+                </ResizablePanel>
+              )}
+            </ResizablePanelGroup>
+          </>
+        )}
+        { overallPage === 'settings' && (
+          <Settings onBackClicked={() => setOverallPage('collection')} />
+        )}
+        <div className={`mt-auto`}>
           <NowPlaying libraries={libraries || new Map<String, Library>()} onPlay={(song) => setNowPlayingId(song?.id)} />
         </div>
-      )}
-      { overallPage === 'settings' && (
-        <Settings onBackClicked={() => setOverallPage('collection')} />
-      )}
-    </QueueContextProvider>
-    
+        
+      </div>
+    </QueueContextProvider>  
   )
 }
