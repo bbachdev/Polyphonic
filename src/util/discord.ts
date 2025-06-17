0a613bc54976275cf6c74d67ca520a2f2609ce0e
bbachdev <bbach7139@gmail.com>
2025-06-17 15:32:58 -0500

    Begin Discord RP process

--- src/util/discord.ts
+++ src/util/discord.ts
@@ -1,44 +1,25 @@
 import { Song } from '@/types/Music';
-import { DiscordSDK } from "@discord/embedded-app-sdk";
+import { Client } from "discord-rpc";
 
-const DISCORD_CLIENT_ID = import.meta.env.VITE_DISCORD_CLIENT_ID
+const clientId = process.env.DISCORD_APPLICATION_ID
 
-// Instantiate the SDK
-const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+const rpc = new Client({
+  transport: "ipc"
+})
 
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is ready");
-});
+export async function setDiscordActivity(song: Song) {
+  if(!clientId || !rpc) {
+    return
+  }
 
-export async function setupDiscordSdk() {
-  await discordSdk.ready();
-}
-
-export async function authorize() {
-  const { code } = await discordSdk.commands.authorize({
-    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-    response_type: "code",
-    state: "",
-    prompt: "none",
-    scope: [
-      "identify",
-      "rpc.activities.write"
-    ],
-  });
-
-  console.log("Code", code);
-}
-
-export async function setActivityStatus(song: Song) {
-  await discordSdk.commands.setActivity({
-    activity: { 
-      type: 2,
+  try {
+    await rpc.setActivity({
       details: song.title,
       state: song.artist_name,
-      assets: {
-        large_image: song.cover_art,
-        large_text: 'Listening to a track',
-      }
-    }
-  });
+      startTimestamp: new Date(),
+      endTimestamp: new Date(Date.now() + song.duration),
+    })
+  }catch(e){
+    console.error(e)
+  }
 }
